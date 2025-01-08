40a52186209680de7d9e3bf2c435379b675e9a0b
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-01-08 08:15:47 +0000

    Class 1: Variables

---
+++ Variables/index.js
@@ -0,0 +1,20 @@
+// Variables
+// Variables are container for storing data
+var name = 'John Doe';
+let name1 = 'Tayo';
+var name = 'tolu';
+const age = 10;
+
+
+var name = 'tolu';
+console.log(name)
+// const age = 16 ;    
+
+console.log(age)
+
+// Types of error
+// Data Types
+// Functions
+name1 = 'titi';
+// let name1 = 'sope'
+console.log(name1);
