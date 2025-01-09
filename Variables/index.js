dea8766d3eb5dc5e29b0f66d1757f58e9d7ac94a
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-01-09 09:01:13 +0000

    Class 2: More on variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,5 +1,10 @@
+// Javascript is an high level , paradiagm and object oriented programming language.
+
 // Variables
+
 // Variables are container for storing data
+// Types of variables : var, let and const
+
 var name = 'John Doe';
 let name1 = 'Tayo';
 var name = 'tolu';
@@ -12,9 +17,13 @@ console.log(name)
 
 console.log(age)
 
-// Types of error
-// Data Types
-// Functions
+ 
 name1 = 'titi';
 // let name1 = 'sope'
 console.log(name1);
+
+// https://github.com/MashobTechie/JavascriptClass.git (Git URL)
+
+// Assignment on variables
+// Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
---
+++ new.js
@@ -0,0 +1,49 @@
+// Writing  conventions
+// 1. camelCase eg newAge
+// 2. PascalCase eg NewAge
+// 3. snake_case
+//  a. small snake_case eg new_age
+// b. big snake_case eg New_Age or NEW_AGE
+
+// Variables and Values
+// firstname is mariam
+// lastname is Abdulkareem
+
+// A variable is a container for storing data
+// Ways of declaring variables
+// a. const
+// b. let
+// c. var
+
+
+// Const : used for declaring variables that cannot be changed.
+// cannot be redeclared or reassign
+// eg:
+const nationality = 'Nigeria'
+// console.log(nationality)
+
+// let : used fo declaring variables. They cannot be redeclared but can be reassigned
+// eg:
+let name = 'Tayo'
+console.log(name)
+// Re-assigned a value to variable name
+ name= 'Tolu';  
+ console.log(name)
+
+//  var: used for declaring variables. they can be redecalred and also, be reassigned
+var age = 30;
+console.log(age);
+// reassigned age to be 50
+age = 50;
+console.log(age)
+// redeclarig age to be 70
+var age = 70;
+console.log(age)
+
+// When naming variables:
+// 1. You cannot use number to start the name of your variable
+// eg:
+//  let 3girlsage =90;
+
+// 2. You can use numbers, letters and underscore(_) at the middle but you can't use & to start a variable name .
+
