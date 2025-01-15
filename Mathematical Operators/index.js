cce3c34c397e1cd5d251415cabaa244c83b356eb
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-01-15 11:52:40 +0000

    Lecture 4: Mathematical Operations

---
+++ Mathematical Operators/index.js
@@ -0,0 +1,36 @@
+// Addition Operator: it is denoted by + 
+let firstNumber = 10;
+let secondNumber = 20;
+
+console.log(firstNumber + secondNumber)
+
+// Subtraction Operator: it is denoted by -
+console.log(firstNumber - secondNumber)
+
+// Multiplication Operator: it is denoted by *
+console.log(firstNumber * secondNumber)
+
+// Division Operator: it is denoted by /
+console.log(firstNumber / secondNumber)
+
+// Modulus Operator: it is denoted by % . It is used to bring out the remainder of a division
+console.log(5 % 2 )
+
+// Increment Operator: it is denoted by ++. It is used to increase the value of a variable by 1
+let newNumber = 5;
+newNumber++
+console.log(newNumber)
+
+// Decreament Operator: Denoted by --. Used to reduce a variable by 1
+let DecreamentNumber = 5;
+DecreamentNumber--
+console.log(DecreamentNumber)
+
+// Exponential Operator: Denoted by **
+console.log(2**3)
+
+// Create a variable with your country's population.
+ // 2. Increase the population of your country by 1 and log the result to the console
+// 3. Finland has a population of 6 million. Does your country have more people than Finland?
+// 4. The average population of a country is 33 million people. Does your country have less people than the average country?
+// 5. Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese
