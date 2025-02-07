4da982746b1097dea083b4b26da286ae2162c2a9
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-02-07 22:41:53 +0000

    added assignments

--- ArrayAssignment/index.js
+++ ArrayAssignment/index.js
@@ -8,7 +8,7 @@ console.log(calcTip(100));
 
 const billValues = [125, 555, 44]
 const ourTips = [calcTip(billValues[0]), calcTip(billValues[1]), calcTip(billValues[2])]
-console.log(ourTips)
+console.log(ourTips);
 
 // Tip for billValue 125 is 18.75
 const expression = `The tip for billValue ${billValues[0]} is ${ourTips[0]}`
@@ -19,7 +19,7 @@ console.log(totalValues)
 console.log(`${totalValues[0]}  is the total value of ${billValues[0]} and ${ourTips[0]}, ${totalValues[1]}  is the total value of ${billValues[1]} and ${ourTips[1]} and lastly ${totalValues[2]}  is the total value of ${billValues[2]} and ${ourTips[2]}`);
 
 
-
+// Assignments
 // Join Array Elements
 // Write a simple JavaScript program to join all elements of the following array into a string.
 // Sample array : myColor = ["Red", "Green", "White", "Black"];
@@ -27,9 +27,8 @@ console.log(`${totalValues[0]}  is the total value of ${billValues[0]} and ${our
 // Empty Array
 // Write a JavaScript  script to empty an array while keeping the original.
 
-// //Check All Numbers Prime
+//Check All Numbers Prime
 // Write a JavaScript program that takes an array of integers and returns false if every number is not prime. Otherwise, return true.
-
 // Test Data :
 // ([2,3,5,7]) -> true
 // ([2,3,5,7,8]) -> false
