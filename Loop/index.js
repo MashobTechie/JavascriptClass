f5b5a12b71eebd1199275a5b23e54cd9112558fe
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-02-12 23:57:55 +0000

    For Loops

---
+++ Loop/index.js
@@ -0,0 +1,14 @@
+// Loops
+// Loops mean repetition
+
+console.log('Carried chair 1')
+console.log('Carried chair 2');
+
+// Loop: 
+// for loop method 
+
+for (let chair = 1; chair <= 10; chair++) {
+    console.log(`Carried chair 1`);
+}
+
+
