23ca552c2791d800f8c0a9d464c65ffb29c83131
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-02-20 14:47:02 +0000

    Progress on DOM

--- DOM_Manipulation/index.js
+++ DOM_Manipulation/index.js
@@ -8,7 +8,7 @@
 
 // document.getElementById('p1').innerHTML = 'Hello World';
 
- 
+
 
 // const ourParagraph  = document.getElementById('p1'); 
 // ourParagraph.innerHTML = 'New OPOPra'
@@ -44,5 +44,89 @@ document.querySelector('p').innerHTML = 'Query Tag'
 // Assignment
 // Write different with no content in them, then use JS to crete a text.
 // Read more on creating, deleting elements with DOM manipulation
-// read on the differences between innerHTML, Text Content, innerText
+// read on the diffe rences between innerHTML, Text Content, innerText
 // Read on the differences between query selector and querySelectorAll
+
+
+
+
+
+const ourP = document.getElementsByClassName('p2');
+
+ 
+
+Array.from(ourP).forEach(el => {
+    el.innerHTML = 'Our Paragraphs';
+});
+
+
+document.getElementById('newP').textContent = 'Hello World';
+
+
+
+// Using innerHTML: This brings everything you passs as html.
+const ourDiv = document.querySelector('#ourDiv');
+ourDiv.innerHTML = '<h1>Our Div</h1>'
+
+// Using textContent: This brings everything you passs as text.Same as textContent
+
+const newDiv = document.querySelector('#newDiv');
+newDiv.textContent = '<h1>Our Div</h1>';
+
+newDiv.style.color = 'red';
+newDiv.style.display = 'block'
+
+
+// Using innerText: This brings everything you passs as text. Same as textContent
+
+
+// create elements: Use the createElement method to create an element. It collects the tag as the argument.
+const newElement = document.createElement('h1');
+newElement.textContent = 'This is our new element 2';
+// After creating the element, you can display it by appending to the body or any other element.
+document.body.appendChild(newElement)
+
+function createTask() {
+    const newTask = document.createElement('div');
+    newTask.innerHTML = `
+    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>John Doe</td>
+                                <td>25</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jane Smith</td>
+                                <td>30</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Mike Johnson</td>
+                                <td>28</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                
+    
+    `;
+    document.body.appendChild(newTask)
+
+}
+
+
+
+// Deleting an element: Use the remove child method to delete an element. It collects the element as the argument.
+document.body.removeChild(newElement)
+
+
+
+// Read more on events and event handler on JS.
