744a7cdf87e4af7c1af9af809a6f061429b30d6d
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-02-25 10:01:49 +0000

    added to-do list assignment

--- new.js
+++ new.js
@@ -1,102 +1,123 @@
-// Writing  conventions
-// 1. camelCase eg newAge
-// 2. PascalCase eg NewAge
-// 3. snake_case
-//  a. small snake_case eg new_age
-// b. big snake_case eg New_Age or NEW_AGE
+// // Writing  conventions
+// // 1. camelCase eg newAge
+// // 2. PascalCase eg NewAge
+// // 3. snake_case
+// //  a. small snake_case eg new_age
+// // b. big snake_case eg New_Age or NEW_AGE
 
-// Variables and Values
-// firstname is mariam
-// lastname is Abdulkareem
+// // Variables and Values
+// // firstname is mariam
+// // lastname is Abdulkareem
 
-// A variable is a container for storing data
-// Ways of declaring variables
-// a. const
-// b. let
-// c. var
-
-
-// Const : used for declaring variables that cannot be changed.
-// cannot be redeclared or reassign
-// eg:
-const nationality = 'Nigeria'
-// console.log(nationality)
-
-// let : used fo declaring variables. They cannot be redeclared but can be reassigned
-// eg:
-let name = 'Tayo'
-console.log(name);
-
-// Re-assigned a value to variable name
-name = 'Tolu';
-console.log(name)
-
-//  var: used for declaring variables. they can be redecalred and also, be reassigned
-var age = 30;
-console.log(age);
-// reassigned age to be 50
-age = 50;
-console.log(age)
-// redeclaring variable age to be 70
-var age = 70;
-console.log(age);
+// // A variable is a container for storing data
+// // Ways of declaring variables
+// // a. const
+// // b. let
+// // c. var
 
 
+// // Const : used for declaring variables that cannot be changed.
+// // cannot be redeclared or reassign
+// // eg:
+// const nationality = 'Nigeria'
+// // console.log(nationality)
 
-// When naming variables:
-// 1. You cannot use number to start the name of your variable
-// eg:
-//  let 3girlsage =90;
+// // let : used fo declaring variables. They cannot be redeclared but can be reassigned
+// // eg:
+// let name = 'Tayo'
+// console.log(name);
 
-// 2. You can use numbers, letters and underscore(_) at the middle but you can't use & to start a variable name .
+// // Re-assigned a value to variable name
+// name = 'Tolu';
+// console.log(name)
 
-// 3. You can't use these keywords to name a varibale : let,const , var, function
+// //  var: used for declaring variables. they can be redecalred and also, be reassigned
+// var age = 30;
+// console.log(age);
+// // reassigned age to be 50
+// age = 50;
+// console.log(age)
+// // redeclaring variable age to be 70
+// var age = 70;
+// console.log(age);
 
 
 
-// Constructingt sentences with variables.
-// Create a variable for firstname, lastname and age. 
-// Construct a sentence with my name is (firstname) and your (lastname). I am (age) years old.
+// // When naming variables:
+// // 1. You cannot use number to start the name of your variable
+// // eg:
+// //  let 3girlsage =90;
 
-let firstname = 'Mariam';
-let lastname = 'Abdulkareem';
-let myAge = 40;
+// // 2. You can use numbers, letters and underscore(_) at the middle but you can't use & to start a variable name .
 
-console.log('My name is ' + firstname, lastname);
-// I am 40 years old
-console.log('I am ' + myAge + 'years old')
+// // 3. You can't use these keywords to name a varibale : let,const , var, function
 
 
-// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
-// Your tasks:
-// 1. Store Mark's and John's mass and height in variables
-// 2. Calculate both their BMIs using the formula (you can even implement both versions)
-// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John
 
-// Solution
-let markHeight = 6;
-let markMass = 70;
-let johnHeight = 5;
-let johnMass = 60;
+// // Constructingt sentences with variables.
+// // Create a variable for firstname, lastname and age. 
+// // Construct a sentence with my name is (firstname) and your (lastname). I am (age) years old.
 
-let markBMI = markMass / (markHeight ** 2);
-console.log('markBMI is ' + markBMI);
-let johnBMI = johnMass / (johnHeight ** 2);
-console.log('johnBMI is ' + johnBMI);
+// let firstname = 'Mariam';
+// let lastname = 'Abdulkareem';
+// let myAge = 40;
 
-if (markBMI > johnBMI) {
-    console.log(true)
-} else {
-    console.log(false)
-}
+// console.log('My name is ' + firstname, lastname);
+// // I am 40 years old
+// console.log('I am ' + myAge + 'years old')
 
-// console.log('MarkBMI is ' + markBMI); 
-// first way if writing statement in the console.log
 
-// second way of writing statement in the console.log
-// Using back-tick(``) and dollar-sign($) and {}. This is called string literals
+// // BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+// // Your tasks:
+// // 1. Store Mark's and John's mass and height in variables
+// // 2. Calculate both their BMIs using the formula (you can even implement both versions)
+// // 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John
 
-console.log(`MarkBMI is ${markBMI}`);
+// // Solution
+// let markHeight = 6;
+// let markMass = 70;
+// let johnHeight = 5;
+// let johnMass = 60;
+
+// let markBMI = markMass / (markHeight ** 2);
+// console.log('markBMI is ' + markBMI);
+// let johnBMI = johnMass / (johnHeight ** 2);
+// console.log('johnBMI is ' + johnBMI);
+
+// if (markBMI > johnBMI) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// // console.log('MarkBMI is ' + markBMI); 
+// // first way if writing statement in the console.log
+
+// // second way of writing statement in the console.log
+// // Using back-tick(``) and dollar-sign($) and {}. This is called string literals
+
+// console.log(`MarkBMI is ${markBMI}`);
 
 
  
+
+// // Assignment: Build a Simple To-Do List Using JavaScript
+// Objective:
+// Create an interactive to-do list using JavaScript and the DOM.
+
+// Instructions:
+// Create an HTML file with:
+
+// An input field (<input> with id="taskInput") for users to enter tasks.
+// A button (<button> with id="addTaskBtn") to add tasks.
+// An unordered list (<ul> with id="taskList") to display tasks.
+// Write JavaScript to:
+
+// Capture the task input when the button is clicked.
+// Add the task as a list item (<li>) inside the task list.
+// Allow users to mark tasks as completed by clicking on them (toggle a CSS class).
+// Add a "Delete" button next to each task to remove it from the list.
+// Bonus (Optional):
+// Style the list using CSS for a better user experience.
+// Prevent empty tasks from being added.
+// Ensure tasks are displayed in a structured format.
