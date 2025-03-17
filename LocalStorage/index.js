dab024273c3a7db99e65285a4a6d426c2ee9cdcd
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-03-17 08:53:14 +0000

    local storage

---
+++ LocalStorage/index.js
@@ -0,0 +1,25 @@
+
+// To save /set item : 
+localStorage.setItem('task', 'Go to bed')
+// It'll have two props in key/value pair.
+// the key (task) and the value (teh task value itself)
+
+/// To get/retrieve: It'll be one which is the key you used to save it
+localStorage.getItem('task')
+
+// To remove one item
+localStorage.removeItem('task')
+
+localStorage.setItem('task1', 'Go to bed')
+localStorage.setItem('task2', 'Go to spa')
+localStorage.setItem('task3', 'Go to work')
+
+
+// Tpo clear all items in the local storage
+localStorage.clear()
+
+
+
+
+// Assignment 
+// Add local storage to your to do list
